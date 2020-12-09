26d9044f1cf6bf5da3a1b1766552a9f024e95bdb
Benoit.Spriet <benoit.spriet@epitech.eu>
2020-12-09 10:55:07 +0100

    test heroku login

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -47,3 +49,7 @@ Route::get("category/{id}",[CategoryController::class,'showCategory']);
  
 //auth
 Route::post("save",[ProductController::class,'testData']);
+
+//Oauth2
+Route::post("register",[AuthController::class,'register']);
+Route::post("login",[AuthController::class,'login']);
