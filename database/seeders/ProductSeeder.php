963074b7dc53a453b029c643fe1e566bbbaea81f
Benoit.Spriet <benoit.spriet@epitech.eu>
2020-12-09 15:42:15 +0100

    heroku ok

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -19,7 +19,7 @@ class ProductSeeder extends Seeder
         $product= new Product;
         $product->name = "marwallou";
         $product->description = "le meilleur fromage que le nord ait jamais crée";
-        $product->image = "";
+        $product->image = "https://images.unsplash.com/photo-1486297678162-eb2a19b0a32d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1353&q=80";
         $product->price = 11.90;
         $product->stock = 67;
         $product->measure_unit = "pièce";
@@ -43,7 +43,7 @@ class ProductSeeder extends Seeder
         $product= new Product;
         $product->name = "brebious";
         $product->description = "5 fois vainqueur du meilleur fromage de brebis au monde";
-        $product->image = "";
+        $product->image = "https://images.unsplash.com/photo-1517999586990-2126f2b0c97d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80";
         $product->price = 30;
         $product->stock = 40;
         $product->measure_unit = "kg";
@@ -55,7 +55,7 @@ class ProductSeeder extends Seeder
         $product= new Product;
         $product->name = "el famoso";
         $product->description = "un assortiment de fromage si bon que vous ne pourrez plus vous en passer";
-        $product->image = "";
+        $product->image = "https://images.unsplash.com/photo-1541529086526-db283c563270?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80";
         $product->price = 67.90;
         $product->stock = 25;
         $product->measure_unit = "pièce";
@@ -67,7 +67,7 @@ class ProductSeeder extends Seeder
         $product= new Product;
         $product->name = "yagurt";
         $product->description = "un yaourt si bon que même la laitière veut la recette";
-        $product->image = "";
+        $product->image = "https://images.unsplash.com/photo-1574473322441-dae4fd56feef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80";
         $product->price = 1.30;
         $product->stock = 83;
         $product->measure_unit = "pièce";
@@ -79,7 +79,7 @@ class ProductSeeder extends Seeder
         $product= new Product;
         $product->name = "chevrette";
         $product->description = "excellent fromage de chèvre";
-        $product->image = "";
+        $product->image = "https://images.unsplash.com/photo-1558221693-89a565d7ecd8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80";
         $product->price = 11.10;
         $product->stock = 32;
         $product->measure_unit = "kg";
@@ -91,7 +91,7 @@ class ProductSeeder extends Seeder
         $product= new Product;
         $product->name = "le marvello";
         $product->description = "un plateau de fromage parfait pour offrir";
-        $product->image = "";
+        $product->image = "https://images.unsplash.com/photo-1561339429-d5da4e6e9105?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80";
         $product->price = 77.80;
         $product->stock = 28;
         $product->measure_unit = "pièce";
@@ -103,7 +103,7 @@ class ProductSeeder extends Seeder
         $product= new Product;
         $product->name = "danote";
         $product->description = "un yaourt cremeux et onctueux";
-        $product->image = "";
+        $product->image = "https://images.unsplash.com/photo-1564149503905-7fef56abc1f2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80";
         $product->price = 1.40;
         $product->stock = 72;
         $product->measure_unit = "pièce";
@@ -115,7 +115,7 @@ class ProductSeeder extends Seeder
         $product= new Product;
         $product->name = "biquette";
         $product->description = "fromage de chèvre parfait pour des tartines grillées";
-        $product->image = "";
+        $product->image = "https://images.unsplash.com/photo-1529312266912-b33cfce2eefd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80";
         $product->price = 8.30;
         $product->stock = 47;
         $product->measure_unit = "pièce";
@@ -127,7 +127,7 @@ class ProductSeeder extends Seeder
         $product= new Product;
         $product->name = "brebete";
         $product->description = "fromage de brebis a partagé entre amis";
-        $product->image = "";
+        $product->image = "https://images.unsplash.com/photo-1599932677968-877d85f64a12?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=618&q=80";
         $product->price = 26;
         $product->stock = 37;
         $product->measure_unit = "kg";
@@ -137,9 +137,9 @@ class ProductSeeder extends Seeder
 
         //11
         $product= new Product;
-        $product->name = "le familial";
-        $product->description = "un plateau de fromage pour toute la famille";
-        $product->image = "";
+        $product->name = "le classico";
+        $product->description = "un plateau de fromage parfait pour une soirée tranquille";
+        $product->image = "https://images.unsplash.com/photo-1551790629-9d5c2d781d8b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1399&q=80";
         $product->price = 61.30;
         $product->stock = 22;
         $product->measure_unit = "pièce";
@@ -151,7 +151,7 @@ class ProductSeeder extends Seeder
         $product= new Product;
         $product->name = "ativia";
         $product->description = "un yaourt bon pour le corps et la santé";
-        $product->image = "";
+        $product->image = "https://images.unsplash.com/photo-1562114808-b4b33cf60f4f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80";
         $product->price = 0.90;
         $product->stock = 73;
         $product->measure_unit = "pièce";
