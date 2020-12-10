1241a50606e0a4d8098a758290b10955628c75a4
Benoit.Spriet <benoit.spriet@epitech.eu>
2020-12-10 15:16:18 +0100

    add cat ok

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,30 +10,27 @@ class CategoryController extends Controller
     //Display a list of all Catgories.
     public function listCategories()
     {
-        return response()->json(Category::all());
-      // return "HelloWorld";
+      return response()->json(Category::all());
     }
 
     //Store a new Category in the Database.
     public function addCategory(Request $req)
    {
-        $category= new Category;
-        $category->name = $req->input('name');
-        $result=$category->save();
+      $category= new Category;
+      $category->name = $req->input('name');
+      $result=$category->save();
 
-        return response()->json([
-            $category,
-            'status' => (bool) $category,
-            'message'=> $result ? 'The Category has been created succesfully' : 'We have encounter an error in the creation of the Category'
-        ]);
+      return response()->json([
+         'status' => (bool) $category,
+         'message'=> $result ? 'The Category has been created succesfully' : 'We have encounter an error in the creation of the Category',
+         $category,
+      ]);
    }
 
    //Display the specified Category.
    public function showCategory($id)
    {
-       
       return response()->json(Category::findOrFail($id));
-       
    }
 
    //Update the specified Category in the Database.
